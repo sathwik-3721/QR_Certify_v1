845aa5a8f98cc538f3f8d9a2108d61b145e55a48
revathipathilanka347 <revathipathilanka347@gmail.com>
2024-11-05 19:50:35 +0530

    fixed get apis

--- client/src/layout/footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <div className="w-full flex items-center text-center justify-center sm:mt-2">
-      Made with ❤️ at Miracle Labs
-    </div>
-  );
-}
-
-export default Footer;
